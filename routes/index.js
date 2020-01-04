6b6f626c68d286dc7ef6793f46d6c800df87911e
Zheng LiangLiang <zhengliangliang1996@gmail.com>
2020-01-04 23:57:18 +0100

    data passing to view fisnihed

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var Twitter = require('twitter');
+
+var router = express.Router();
+
+const T = new Twitter({
+  consumer_key:               'zdjR5WJk0nHm7Y8Td9Cb12vdP'
+  , consumer_secret:          'INlzlyAT3WMMSWF8TC57IsyFzlo7E00heMyWxUrAFR5lM2GLlw'
+  , access_token_key:         '1149832237468332039-VKuQUNCqZjJQM0mfRZ9DzOcw3WYPEF'
+  , access_token_secret:      'x24SVlZTEK1uzVQReVX7jm3s241BZywEHoDMcg3Euf2LE'
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  // Get twiiter data and send back to index page
+  var params = {id:'1'}
+  T.get('trends/place', params, function(err, data, response) {
+      if (!err){
+        var tweets = data;
+        res.render('index', { title: 'Twitter Popularity Tracking', trends:tweets[0].trends})
+        // res.locals = {url:'index', trends: tweets[0].trends};
+        // res.send(tweets[0].trends);
+      } else {
+          console.log(err);
+      }
+  });
+
+  
+});
+
+module.exports = router;
